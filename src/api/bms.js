37d191b1aa9f5024b07186666ea500b89e1a6b4e
xinyuanxiaolin <xinyuanxuaolin1@163.com>
2024-02-17 21:11:08 +0800

    完成商品分类管理模块

--- src/api/bms.js
+++ src/api/bms.js
@@ -12,3 +12,29 @@ import {
 export const Login = (data) => {
   return postAction("/login/bms", data);
 };
+
+/* 分类管理 */
+//添加分类
+export const addCategory = (data) =>{
+  return postAction("/category",data);
+}
+//获取分类详情
+export const getCategory = ()=>{
+  return getAction("/category")
+}
+
+//删除一级分类
+export const deleteOneCategory=(id)=>{
+  return deleteAction(`/category/one/${id}`)
+
+}
+//删除二级分类
+export const deleteTwoCategory=(id)=>{
+  return deleteAction(`/category/two/${id}`)
+
+}
+
+//编辑分类
+export const putCategory=(data)=>{
+  return putAction(`/category`,data)
+}
--- src/views/sortManage/sortManage.vue
+++ src/views/sortManage/sortManage.vue
@@ -1,23 +1,25 @@
 <template>
     <div>
-        <el-row class="oneBar">
+        <el-row class="oneBar" v-for="(item, index) in categoryList" :key="index">
             <el-row justify="space-between" type="flex" class="oneBar-title">
-                <el-col :span="3" class="title-name"><span>手机数码</span></el-col>
+                <el-col :span="3" class="title-name"><span>{{ item.name }}</span></el-col>
                 <el-col :span="6" class="title-icon">
                     <el-row justify="end" type="flex" class="edit-icon">
-                        <el-col :span="3"><i @click="addLevel2(3)" class="iconfont icon-tianjia"></i></el-col>
-                        <el-col :span="3"><i @click="dialogFormVisible = true" style="font-size: 25px"
+                        <el-col :span="3"><i @click="addLevel2(item.id)" class="iconfont icon-tianjia"></i></el-col>
+                        <el-col :span="3"><i @click="editCategory(item, 1)" style="font-size: 25px"
                                 class="iconfont icon-bianji"></i></el-col>
-                        <el-col :span="3"><i class="iconfont icon-shanchu"></i></el-col>
+                        <el-col :span="3"><i @click="deleteTopCategory(item.id)" class="iconfont icon-shanchu"></i></el-col>
                     </el-row>
                 </el-col>
             </el-row>
             <el-row class="oneBar-content">
-                <el-button type="primary">上传<i class="el-icon-close el-icon--right"></i></el-button>
-                <el-button type="primary">上传<i class="el-icon-close el-icon--right"></i></el-button>
-                <el-button type="primary">上传<i class="el-icon-close el-icon--right"></i></el-button>
-                <el-button type="primary">上传<i class="el-icon-close el-icon--right"></i></el-button>
-                <el-button type="primary">上传<i class="el-icon-close el-icon--right"></i></el-button>
+                <el-button type="primary" v-for="(secItem, secIndex) in item.children" :key="secIndex"><span
+                        @click.prevent="editCategory(secItem, 2)">{{ secItem.name + "(" + secItem.goods.length +
+                            ")" }}</span><i @click.prevent="deleteSecCategory(secItem.id)"
+                        class="el-icon-close el-icon--right"></i></el-button>
+                <div style="display: flex; margin-top: 10px; justify-content: center" v-if="item.children.length === 0">
+                    该分类下还没有子栏目
+                </div>
             </el-row>
         </el-row>
         <!-- 新增一级分类 -->
@@ -25,88 +27,177 @@
 
         <!-- 弹出框 -->
         <el-dialog :title="title" :visible.sync="dialogFormVisible">
-            <el-form :model="form">
-                <el-form-item label="分类名称" :label-width="formLabelWidth">
+            <el-form :model="form" :rules="rules" ref="ruleForm">
+                <el-form-item label="分类名称" :label-width="formLabelWidth" prop="name">
                     <el-input v-model="form.name" autocomplete="off"></el-input>
                 </el-form-item>
                 <el-form-item label="图片" :label-width="formLabelWidth">
-                    <el-upload class="avatar-uploader" list-type="picture-card" action="https://jsonplaceholder.typicode.com/posts/"
-                        :show-file-list="false" :on-success="handleAvatarSuccess" :before-upload="beforeAvatarUpload">
-                        <img v-if="form.picture" :src="form.picture" class="avatar">
+                    <el-upload class="avatar-uploader" list-type="picture-card"
+                        action="https://jsonplaceholder.typicode.com/posts/" :show-file-list="false"
+                        :on-success="handleAvatarSuccess" :before-upload="beforeAvatarUpload">
+                        <img v-if="form.picture" :src="form.picture" class="avatar" />
                         <i v-else class="el-icon-plus avatar-uploader-icon"></i>
                     </el-upload>
                 </el-form-item>
 
                 <el-form-item label="级别" :label-width="formLabelWidth">
-                    <span v-if="categoryLevel === 1">一级分类</span>
-                    <span v-if="categoryLevel === 2">二级分类</span>
+                    <span v-if="form.level === 1">一级分类</span>
+                    <span v-if="form.level === 2">二级分类</span>
                 </el-form-item>
             </el-form>
             <div slot="footer" class="dialog-footer">
-                <el-button @click="dialogFormVisible = false">取 消</el-button>
-                <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+                <el-button @click="cancelSumbit">取 消</el-button>
+                <el-button type="primary" @click="confirmSumbit">确 定</el-button>
             </div>
         </el-dialog>
     </div>
 </template>
 
 <script>
+import {
+    getCategory,
+    addCategory,
+    deleteOneCategory,
+    deleteTwoCategory,
+    putCategory,
+} from "@/api/bms";
 export default {
     name: "sortManage",
     data() {
         return {
             dialogFormVisible: false,
-            categoryLevel: 1,
             title: "操作",
-            imageUrl: "",
+            isEdit: false,
+            categoryList: [],
             form: {
+                id: "",
                 name: "",
                 picture: "",
                 level: "",
-                parentId:"",
+                parentId: "",
             },
             formLabelWidth: "120px",
+            rules: {
+                name: [{ required: true, message: "请输入分类名称", trriger: "blur" }],
+            },
         };
     },
+    mounted() {
+        this.init();
+    },
     methods: {
+        //获取分类数据
+        async init() {
+            const res = await getCategory();
+            //    console.log(res);
+            this.categoryList = res.result;
+        },
+
         //添加二级分类
         addLevel2(val) {
             this.dialogFormVisible = true;
-            this.categoryLevel = 2;
+            this.isEdit = false;
             this.title = "添加二级分类";
-            this.level =2;
-            this.parentId =val;
+            this.form.level = 2;
+            this.form.parentId = val;
         },
         //添加一级分类
         addLevel1() {
             this.dialogFormVisible = true;
-            this.categoryLevel = 1;
+            this.isEdit = false;
             this.title = "添加一级分类";
-            this.level =1;
+            this.form.level = 1;
         },
+        //删除一级分类
+        deleteTopCategory(id) {
+            this.$confirm(
+                "是否要删除该分类,若删除其下关联分类和商品也会对应删除?",
+                "删除一级分类"
+            )
+                .then(async (result) => {
+                    await deleteOneCategory(id);
+                    this.$message.success("删除成功");
+                    this.init();
+                })
+                .catch(() => { });
+        },
+        //删除二级分类
+        deleteSecCategory(id) {
+            this.$confirm(
+                "是否要删除该分类,若删除其下关联商品也会对应删除?",
+                "删除二级分类"
+            )
+                .then(async (result) => {
+                    await deleteTwoCategory(id);
+                    this.$message.success("删除成功");
+                    this.init();
+                })
+                .catch(() => { });
+        },
+        //编辑分类
+        editCategory(item, level) {
+            this.title = "修改分类";
+            this.dialogFormVisible = true;
+            this.isEdit = true;
+            //level为1,编辑一级分类,为2,编辑二级分类
+            this.form.level = level;
+            this.form.id = JSON.parse(JSON.stringify(item.id));
+            this.form.name = JSON.parse(JSON.stringify(item.name));
+        },
+        //取消提交表单
+        cancelSumbit() {
+            this.dialogFormVisible = false;
+            this.form = {
+                name: "",
+                picture: "",
+                level: "",
+                parentId: "",
+            };
+        },
+        //提交表单
+        confirmSumbit() {
+            this.$refs["ruleForm"].validate((valid) => {
+                if (valid) {
+                    //做个是不是编辑分类的判断
+                    if (this.isEdit) {
+                        putCategory(this.form);
+                    } else {
+                        addCategory(this.form);
+                    }
+                    this.$message.success("提交成功");
+                    this.init();
+                    this.form = {
+                        name: "",
+                        picture: "",
+                        level: "",
+                        parentId: "",
+                    };
+                    this.dialogFormVisible = false;
+                } else {
+                    // console.log('error submit!!');
+                    return false;
+                }
+            });
+        },
+
         handleAvatarSuccess(res, file) {
             this.form.picture = URL.createObjectURL(file.raw);
         },
         beforeAvatarUpload(file) {
-            console.log(file)
-            const isJPG = file.type === 'image/jpeg';
-            const isPNG = file.type === 'image/png';
+            // console.log(file)
+            const isJPG = file.type === "image/jpeg";
+            const isPNG = file.type === "image/png";
             const isLt2M = file.size / 1024 / 1024 < 2;
             if (!isLt2M) {
-                this.$message.error('上传头像图片大小不能超过 2MB!');
+                this.$message.error("上传头像图片大小不能超过 2MB!");
             }
             if (isJPG) {
                 return isJPG && isLt2M;
-            }
-            else if(isPNG){
+            } else if (isPNG) {
                 return isPNG && isLt2M;
             }
-            this.$message.error('上传头像图片只能是 JPG 格式或者PNG格式!');
-            
-
-            
-        }
-
+            this.$message.error("上传头像图片只能是 JPG 格式或者PNG格式!");
+        },
     },
 };
 </script>
